01dd7f519235097b1d33cbfa7948039ed9ce18c3
patwadeepak <patwa.deepak@gmail.com>
2025-08-24 10:57:36 +0530

    C2 solved finally

--- codeforces/contests-participated/Codeforces Round 1043 (Div. 3)/C2-practice.py
+++ codeforces/contests-participated/Codeforces Round 1043 (Div. 3)/C2-practice.py
@@ -1,50 +1,54 @@
 # C2. The Cunning Seller (hard version)
 
 def decimal_to_arbitrary_base(decimal_num, base):
-    digits = "012"
-    result = ""
+    digits = [0, 1, 2]
+    result = []
     current_num = decimal_num
 
+    min_deals = 0
     while current_num > 0:
         remainder = current_num % base
-        result = digits[remainder] + result
+        result.append(digits[remainder])
         current_num //= base
+        min_deals += result[-1]
 
-    return result
+    return result[::-1], min_deals
 
 
 def solve(n, k):
-    output = list(map(int, list(decimal_to_arbitrary_base(n, 3))))
+    output, min_deals = decimal_to_arbitrary_base(n, 3)
     cost = 0
 
-    for x, ch in enumerate(output):
-        min_deals = sum(output)
-        if min_deals < k and x < len(output)-1:
-            while output[x] > 0 and min_deals+2 <= k:
-                output[x+1] += 3
-                output[x] -= 1
-                min_deals = sum(output)
-
-    N = 0
-    for x, ch in enumerate(output[::-1]):
-        cost += (3**(x+1) + (x * 3**(x-1)))*int(ch)
-        N += (3**(x)) * int(ch)
-
-    if int(N) == int(n) and sum(output) <= k:
-        print(int(cost))
-    else:
+    if min_deals > k:
         print(-1)
+        return
+
+    k -= min_deals
+    for x, ch in enumerate(output):
+        if x < len(output) - 1 and output[x] > 0:
+            if output[x] <= k/2:
+                max_removal = output[x]
+            else:
+                max_removal = k//2
+
+            increase_in_min_deals = max_removal*2
+            increase_in_next_power_deals = max_removal*3
+            output[x] -= max_removal
+            min_deals += increase_in_min_deals
+            output[x+1] += increase_in_next_power_deals
+            k -= increase_in_min_deals
+
+        y = len(output) - x - 1
+        three_x = 3**y
+        cost += ((three_x*3) + (y*three_x/3))*output[x]
+
+    print(int(cost))
 
 
 if __name__ == '__main__':
     t = int(input())
 
-    for i in range(t):
+    for i in range(1, t+1):
         n, k = tuple(map(int, input().split(' ')))
 
         solve(n, k)
-
-"""
-Seems like a correct solution but CF is throwing TLE for now.
-Lets see if we can make it more efficient.
-"""
