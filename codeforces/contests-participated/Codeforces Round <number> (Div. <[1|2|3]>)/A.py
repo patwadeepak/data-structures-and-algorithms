bdc79bda382e5b4ad10a35216da0e52afb5262e8
patwadeepak <patwa.deepak@gmail.com>
2025-08-24 20:05:34 +0530

    template for CF round added

---
+++ codeforces/contests-participated/Codeforces Round <number> (Div. <[1|2|3]>)/A.py
@@ -0,0 +1,13 @@
+# A. <Problem Name>
+
+def solve():
+    pass
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for i in range(1, t+1):
+        n = int(input())
+        # n, k = tuple(map(int, input().split(' ')))
+        solve()
