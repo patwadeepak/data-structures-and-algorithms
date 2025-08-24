8fec9a2010fd2031d90a5186a1da38f0c2d20f83
patwadeepak <patwa.deepak@gmail.com>
2025-08-24 20:00:49 +0530

    progress made in solving problem D

--- codeforces/contests-participated/Codeforces Round 1043 (Div. 3)/D-practice.py
+++ codeforces/contests-participated/Codeforces Round 1043 (Div. 3)/D-practice.py
@@ -29,36 +29,6 @@ while digits_left != 0:
     sum += final_num//(10**digits_left)
     digits_left -= 1
 
-lets go into the details of step - 
-sum from min_number_of_digit_order to last_num
-
-lets say digit_order - 2, min_num = 100 & last_num = 256
-
-ones places = 0, 1, 2, 3, 4, 5, 6         - 7 possibile choices
-2s places   = 0, 1, 2, 3, 4, 5            - 6 possibile choices
-3s plces    = 1, 2 (zero not allowed here)- 2 possibile choices
-
-3s places - 1
-2s places - 0 to 9
-1s places - 0 to 9
-
-45 + (10+45) + 20+45 + 30+45 ... + 90+45
-10 + 20 + 30 + ... + 80 + 90 = 450
-450 + 45 * 10 = 495
-
-10 to 99
-
-a[i] = (a[i-1] * 9) + 45*10**(i-1) = 405 + 450 = 855
-
-855 + 180*1 = 1035
-
-3s places - 2
-2s places - 0 to 4
-1s places - 0 to 9
-
-
-S()
-
 """
 import math
 
@@ -67,42 +37,55 @@ def sumOfDigits(n, a):
         return (n * (n + 1) // 2)
 
     d = int(math.log10(n))
-    p = int(math.ceil(math.pow(10, d)))
+    p = 10**d
     m = n // p
 
     return (m * a[d]) + ((m * (m - 1) // 2) * p) + (m * (1 + n % p)) + sumOfDigits(n % p, a)
 
 
 def solve(n, a):
-    digit_order = 1
-    sum = 0
+    i = 0
+    last_full_num_sum = None
+    last_full_num = None
+    while n > 0:
+        i += 1
 
-    # handle case of single digits
-    if n <= 9:
-        sum += n * (n + 1) / 2
-        return sum
-    n -= 9
+        digit_in_i = i * 9 * 10**(i-1)
+        if digit_in_i <= n:
+            n -= digit_in_i
+        else:
+            if n <= i:
+                last_full_num_sum = 1
+                last_full_num = 10**(i-1) - 1
+            else:
+                last_full_num = (n//i - 1) * 10**(i-1)
+                partial_num_digits = n%i
+                partial_num = last_full_num + 1
+            break
+    
+    total = sumOfDigits(last_full_num, a)
+    if last_full_num_sum is not None:
+        total += last_full_num_sum
+    else:
+        total += sum(map(int, list(str(partial_num)[:partial_num_digits])))
 
-    while True:
-        digit_order += 1
-        # handle case of double digits and further
-        total_digit_count = digit_order * 9 * 10**(digit_order-1)
-        if n <= total_digit_count:
-            last_num = n // digit_order
-            sum += sumOfDigits(last_num, a)
+    print(total)
+
 
 if __name__ == '__main__':
-    "Not the correct solution yet"
+    """
+    There is some logical bug in this solution still.
+    Will attempt this after CF competition today.
+    """
     d = 16 # since n can vary till 10**15
     a = [0] * (d + 1)
     a[0] = 0
     a[1] = 45
 
-    t = int(input())
-
     for i in range(2, d + 1):
         a[i] = a[i - 1] * 10 + 45 * int(math.ceil(math.pow(10, i - 1)))
 
+    t = int(input())
     for i in range(1, t+1):
         n = int(input())
 
