7d323dfa7c26f1e95a3981284303ac819020e242
patwadeepak <patwa.deepak@gmail.com>
2025-08-26 09:18:38 +0530

    D. Chicken Jockey - remain unsolved for now.

--- codeforces/contests-participated/Codeforces Round 1044 (Div. 2)/D-practice.py
+++ codeforces/contests-participated/Codeforces Round 1044 (Div. 2)/D-practice.py
@@ -1,24 +1,50 @@
 # D. Chicken Jockey
 
-def solve(a, n):
-    if n == 1:
-        if a[0] > 0:
-            return a[0]
+def get_damage(a, n, i):
+    d = a[i]
+
+    if i+1 < n:
+        if a[i+1] <= i+1:
+            d += a[i+1]
+            if i+2 < n:
+                d += get_damage(a[i+2:], len(a[i+2:]), 0)
         else:
-            return 0
+            d += i+1
+    return d
 
-    while n > 0:
-        if a[n-1] < 1:
-            a.pop()
-        if a[n-1] == 1 and n <= len(a) - 1:
-            a[n] -= (n-1)
-            return 1 + solve(a[:n-1], len(a[:n-1])) + solve(a[n:], len(a[n:]))
 
-        # last line
-        n -= 1
+def solve(a, n):
+    arrs = [a]
+    a = 0
+    i_arr = 0
+    while arrs:
+        arr = arrs[i_arr]
+        i_max_d = -1
+        max_d = float('-inf')
+        for i in range(len(arr)):
+            d = get_damage(arr, len(arr), i)
+            if max_d < d:
+                max_d = d
+                i_max_d = i
+        a += arr[i_max_d]
 
+        part2 = None
+        if i_max_d+1 < len(arr):
+            arr[i_max_d+1] -= i_max_d+1
+            if arr[i_max_d+1] > 0:
+                part2 = arr[i_max_d+1:]
+            elif i_max_d+2 < len(arr):
+                part2 = arr[i_max_d+2:]
+
+        arrs.pop(i_arr)
+        part1 = arr[:i_max_d]
+        if part1:
+            arrs.append(part1)
+        if part2:
+            arrs.append(part2)
 
 if __name__ == '__main__':
+    "NOT A SOLUTION"
     t = int(input())
 
     for i in range(1, t+1):
@@ -27,4 +53,8 @@ if __name__ == '__main__':
         print(solve(a, n))
 """
 This looks like a very very good problem to solve.
+"""
+
+"""
+I realize, I need to polish my DP then attempt this again later.
 """
