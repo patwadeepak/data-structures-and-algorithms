e40b34dae2f9bb2d0f5907682b77d60f5ace5f36
patwadeepak <patwa.deepak@gmail.com>
2025-09-17 20:00:50 +0530

    WIP: I have the solution but didn't has the time to code this up yet.

--- codeforces/contests-participated/Educational Codeforces Round 182 (Rated for Div. 2)/B.py
+++ codeforces/contests-participated/Educational Codeforces Round 182 (Rated for Div. 2)/B.py
@@ -1,28 +1,34 @@
-from collections import Counter
-
 def solve(a, n):
     if n <= 1:
         return 0
 
-    c = Counter(a)
-    multi_zeros = c[0] > 1
-
-    if c[0] == n:
-        return n
-
+    multi_zeros = False
+    zero_count = 0
+    for num in a:
+        if num == 0:
+            zero_count += 1
+            if zero_count > 1:
+                multi_zeros = True
+                break
+    
     l = 0
     r = n-1
 
-    moved = True
-    while l <= r and moved and l <= n-1 and r >= 0:
-        moved = False
-        if a[l] == l+1 or (a[l] == 0 and not multi_zeros):
+    while l <= r:
+        if a[l] == l+1:
             l += 1
-            moved = True
-
+        elif a[l] == 0:
+            if multi_zeros:
+                break
+            else:
+                pass
+                
+#  or (a[l] == 0 and not multi_zeros):
+    while l <= r and r >= 0:
         if a[r] == r+1 or (a[r] == 0 and not multi_zeros):
             r -= 1
-            moved = True
+        else:
+            break 
     
     return max(r - l + 1, 0)
 
@@ -34,3 +40,7 @@ if __name__ == '__main__':
         a = list(map(int, input().split(' ')))
         op = solve(a, n)
         print(op)
+
+"""
+WIP
+"""
