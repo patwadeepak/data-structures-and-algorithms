859e05db0a057ba0c569ed58be0a7f34998964f8
patwadeepak <patwa.deepak@gmail.com>
2025-09-19 10:27:59 +0530

    solved topological sort problem :)

--- codeforces/contests-participated/Codeforces Round 1051 (Div. 2)/C-practice.py
+++ codeforces/contests-participated/Codeforces Round 1051 (Div. 2)/C-practice.py
@@ -1,32 +1,51 @@
 from collections import defaultdict
 
-def topsort(n, g, root):
-    return []
+def dfs(n, g, node, s, vis, arr):
+    if node in vis:
+        return
+    s.append(node)
+    childs = g.get(node, [])
+    for child in childs:
+        dfs(n, g, child, s, vis, arr)
+    item = s.pop()
+    vis.add(item)
+    arr.append(item)
+
+def topsort(n, g, roots):
+    op = []
+
+    s = []
+    vis = set()
+    for root in roots:
+        dfs(n, g, root, s, vis, op)
+    return op[::-1]
 
 if __name__ == '__main__':
     t = int(input())
 
     for i in range(1, t+1):
         g = defaultdict(list)
-        all_set = set()
-        not_root = set()
+        all = set()
+        child = set()
         n = int(input())
         for j in range(n-1):
             u, v, x, y = list(map(int, input().split(' ')))
 
-            all_set.add(u)
-            all_set.add(v)
+            all.add(u)
+            all.add(v)
             
-            if x > y:
+            if x <= y:
                 g[u].append(v)
-                not_root.add(v)
+                child.add(v)
             else:
                 g[v].append(u)
-                not_root.add(u)
-        
-        root = list(all_set - not_root)[0]
-        print(topsort(n, g, root))
+                child.add(u)
 
-"""
-WIP: Need to work on the topological sort for this problem
-"""
+        # multiple roots possible since not a binary tree but a generic tree
+        roots = all - child
+        op = topsort(n, g, roots)
+
+        ans = [0]*n
+        for i, v in enumerate(op, start=1):
+            ans[v-1] = i
+        print(*ans)
