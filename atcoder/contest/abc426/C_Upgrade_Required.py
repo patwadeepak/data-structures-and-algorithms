312b6ab8003cdc184628c4467900f7c1c5c0d305
patwadeepak <patwa.deepak@gmail.com>
2025-10-05 07:17:57 +0530

    Although it was not an unfair python3 TLE but I decided to switch anyway

--- atcoder/contest/abc426/C_Upgrade_Required.py
+++ atcoder/contest/abc426/C_Upgrade_Required.py
@@ -25,4 +25,12 @@ but it was giving TLE.
 After looking at the Solutions by TOP LGMs,
 it seems that they implemented exact same solution
 but since it was C++, it was fast enough to AC.
+
+Update -
+It was not unfair python TLE but it sure looked like it.
+This is time my algo was too slow.
+
+but for better or worse, I have decided to join the majority
+and pick up C++ again. As I don't want to keep second guessing
+if TLE was due to python slowness.
 """
