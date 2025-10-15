593e39d9a11a6d2f51fde9718decc655cfe868a7
patwadeepak <patwa.deepak@gmail.com>
2025-10-15 07:45:40 +0530

    super simple c++ template to start with

---
+++ bits/cpp_template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int solve() {
+   return 0; 
+}
+
+int main () {
+    int t; cin >> t;
+    while (t--) {
+        int  ans = solve();
+        cout << ans << '\n';
+    }
+    return 0;
+}
