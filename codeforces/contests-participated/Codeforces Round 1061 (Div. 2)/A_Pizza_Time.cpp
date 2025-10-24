cd5e40c95da22ef304583b10b3a7f6cbf4bdf1ff
patwadeepak <patwa.deepak@gmail.com>
2025-10-24 23:33:50 +0530

    bad contest. solved only 1

---
+++ codeforces/contests-participated/Codeforces Round 1061 (Div. 2)/A_Pizza_Time.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define FASTIO ios::sync_with_stdio(0);cin.tie(0)
+
+typedef long long ll;
+
+void solve() {
+    ll n; cin >> n;
+
+    ll hao = 0;
+    while (n >= 3) {
+        hao += n/3;
+        n = n/3 + n%3;
+    }
+
+    cout << hao << endl;
+    return;
+}
+
+int main () {
+    FASTIO;
+    ll t; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
