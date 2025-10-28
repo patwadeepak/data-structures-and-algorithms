03689cec36afde3c7c9acb05c585e237f1a9db31
patwadeepak <patwa.deepak@gmail.com>
2025-10-28 07:20:59 +0530

    found a less bloated decent c++ template

---
+++ bits/template_pick_useful_stuff.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1e9
+#define INF64 9223372036854775807LL
+#define NINF64 -9223372036854775807LL
+#define MOD 998244353
+#define MOD2 1000000007
+#define PI 3.141592653589793238462
+#define int int64_t
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define si set<int>
+#define msi multiset<int>
+#define mpiii map<pair<int, int>, int>
+#define mii map<int, int>
+#define pqi priority_queue<int>
+#define vvi vector<vector<int>>
+#define vc vector<char>
+#define vpii vector<pair<int, int>>
+#define vpivi vector<pair<int, vector<int>>>
+#define pii pair<int, int>
+#define vb vector<bool>
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+template<typename T>
+void print(const T &x){
+    cout << x << endl;
+}
+
+template<typename T>
+void print(const vector<T> &v){
+    for(const auto &x : v) cout << x << ' ';
+    cout << endl;
+}
+
+void print(const string &s){
+    cout << s << endl;
+}
+//******************************* Start ******************************/
+void solve(){
+
+}
+//******************************** End ******************************/
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--){
+        solve();    
+    }
+    return 0;
+}
+
